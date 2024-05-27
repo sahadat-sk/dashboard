b2c52e7e74e14e085dd7d284c3d6a484b349715e
sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>
2024-05-27 07:55:55 +0530

    add: go live section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,33 @@
 import { Chart } from "@/components/chart";
 import { ProfileSetup } from "@/components/profilesetup";
+import { Button } from "@/components/ui/button";
+import { Edit, Eye } from "lucide-react";
 
 export default function Dashboard() {
   return (
     <>
-      <div className="flex items-center">
-        <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
+      <div className="flex items-center w-full h-20 bg-secondary rounded-lg border flex items-center justify-between px-8">
+        <div className="flex gap-2 text-xl">
+          <p className="text-black/60">
+            Your profile is live at{" "}
+            <span className="text-black font-semibold underline">
+              yournexthire.ai/kushagra
+            </span>
+          </p>
+          <button className="flex items-center gap-1">
+            <Edit /> <p className="text-sm underline">Edit</p>
+          </button>
+        </div>
+        <div className="flex items-center gap-4">
+          <button className="flex items-center gap-1">
+            <Eye />
+            <p className="underline semibold">Preview</p>
+          </button>
+          <Button className="px-8 ">Go Live</Button>
+        </div>
       </div>
       <div
-        className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
+        className="flex flex-1 items-center justify-center rounded-lg   "
         x-chunk="dashboard-02-chunk-1"
       >
         <ProfileSetup />
--- src/components/ui/navbarMobile.tsx
+++ src/components/ui/navbarMobile.tsx
@@ -1,37 +1,17 @@
 import Link from "next/link";
 import {
-  Bell,
-  CircleUser,
   Home,
   LineChart,
   Menu,
   Package,
   Package2,
-  Search,
   ShoppingCart,
   Users,
 } from "lucide-react";
 
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Input } from "@/components/ui/input";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { SideNav } from "@/components/ui/navbar";
 
 export function NavBarMobile() {
   return (
